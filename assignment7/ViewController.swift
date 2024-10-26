0b609d1b2b0421f11ab9156bfcd6e86182fad455
ukn0wme <ukn0wme.design@gmail.com>
2024-10-26 06:06:01 +0500

    Initial Commit

---
+++ assignment7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  assignment7
+//
+//  Created by Daniyal Nurgazinov on 26.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ assignment7Tests/assignment7Tests.swift
@@ -0,0 +1,17 @@
+//
+//  assignment7Tests.swift
+//  assignment7Tests
+//
+//  Created by Daniyal Nurgazinov on 26.10.2024.
+//
+
+import Testing
+@testable import assignment7
+
+struct assignment7Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
